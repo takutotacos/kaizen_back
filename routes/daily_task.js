e1c693c60d907d474bcae2e13c2df2b5d5e66d7a
takutotacos <s.takuto0214@gmail.com>
2018-12-19 14:24:38 -0600

    logger added

--- routes/daily_task.js
+++ routes/daily_task.js
@@ -1,21 +1,27 @@
 let DailyTask = require('../models/DailyTask');
 let express = require('express');
 let router = express.Router();
+let log4js = require('log4js');
+let logger = log4js.getLogger();
+logger.level = 'debug';
 
 router.get('/:year/:month/:day', (req, res) => {
+  logger.debug(req.params);
+
   let {year, month, day} = req.params;
   let request_params = {
     target_date: `${year}/${month}/${day}`,
     user: req.user.id
   };
-  console.log(`the request for daily_schedule of ${year}/${month}/${day}`);
+
+  logger.debug(`the request for daily_schedule of ${year}/${month}/${day}`);
 
   DailyTask
     .find(request_params)
     .populate('ticket')
     .exec((err, daily_tasks) => {
     if (err) {
-      console.log('error fetching daily_tasks');
+      logger.debug('error fetching daily_tasks');
       daily_tasks = [];
     }
 
@@ -24,9 +30,9 @@ router.get('/:year/:month/:day', (req, res) => {
 });
 
 router.post('/:year/:month/:day', (req, res) => {
-  console.log('DEBUG: ' + req.body);
-  let {year, month, day} = req.params;
+  logger.debug(req.body);
 
+  let {year, month, day} = req.params;
   let daily_task_param = new DailyTask({
     target_date: `${year}/${month}/${day}`,
     start_date: req.body['start_date'],
@@ -36,22 +42,31 @@ router.post('/:year/:month/:day', (req, res) => {
     ticket_title: req.body['ticket_title'],
     user: req.user.id
   });
-  console.log(daily_task_param);
+
+  logger.debug(daily_task_param);
 
   let newDailyTask = new DailyTask(daily_task_param);
   let error = newDailyTask.validateSync();
   if (error !== undefined) {
+    logger.debug(error.message);
     return res.status(422).json({errors: error.message});
   }
 
   newDailyTask
     .save()
-    .then(daily_task => res.json(daily_task))
-    .catch(error => res.status(422).send(error.errorMessage));
+    .then(daily_task => {
+      logger.debug(daily_task);
+      res.json(daily_task);
+    })
+    .catch(error => {
+      logger.debug(error.errorMessage);
+      res.status(422).send(error.errorMessage)
+    });
 });
 
 router.patch('/:year/:month/:day', (req, res) => {
-  console.log('DEBUG: ' + req.body);
+  logger.debug(req.body);
+
   let {year, month, day} = req.params;
   let schedule_id = req.body['schedule_id'];
   let daily_task_param = {
@@ -62,31 +77,35 @@ router.patch('/:year/:month/:day', (req, res) => {
     ticket: req.body['ticket_id'],
     user: req.user.id
   };
-  console.log(daily_task_param);
+
+  logger.debug(daily_task_param);
 
   DailyTask.findOneAndUpdate({_id: schedule_id}, daily_task_param, {upsert: true, new: true}, (err, raw) => {
     if (err) {
+      logger.debug(err.message);
       res.status(422).send(err.errorMessage);
       return;
     }
 
-    console.log(raw);
+    logger.debug(raw);
     res.json(raw);
   });
 });
 
 router.delete('/:year/:month/:day/:schedule_id', (req, res) => {
-  console.log("-------------------");
-  let {year, month, day, schedule_id} = req.params;
+  logger.debug(req.params);
 
+  let {year, month, day, schedule_id} = req.params;
   let params = {
     target_date: `${year}/${month}/${day}`,
     _id: schedule_id
   };
-  console.log(params);
+
+  logger.debug(params);
 
   DailyTask.deleteOne(params, (err) => {
     if (err) {
+      logger.debug(err.message);
       res.status(404).send(err.errorMessage);
       return;
     }
